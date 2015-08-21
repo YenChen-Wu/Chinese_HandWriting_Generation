a237c1a552f73dac53665e26e4fec1cf07b29acb
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-08-21 22:41:52 +0800

    train data

--- scripts/get_3000_words.sh
+++ scripts/get_3000_words.sh
@@ -1,18 +1,15 @@
-#!/bin/bash -e
-cd ../imgs
+#!/bin/bash -ex
+cd ../train_data
 police=7
 font=WenQuanYi-Micro-Hei-Regular     # use convert
 #font=type${police}					  # use chinesetools php
 dir=${font}_3000
 rm -rf $dir && mkdir $dir && cd $dir
 
-while read -n 1 word 
+while read num word junk
 do
-	if [ ! $word == "　" ];then
-		echo $word
-		convert -font $font -pointsize 50 label:$word ${word}.png
-		#wget http://www.chinesetools.eu/names/gen_boutons.php?text=${word}&amp;s=76&amp;police=${police}&amp;dispo=1   # disposition=1(horizontal), 2(vertical) WGET can't run
-		#curl "http://www.chinesetools.eu/names/gen_boutons.php?text=${word}&s=76&police=${police}&dispo=1" -o ${word}.png 2> /dev/null
-	fi
-done < ../../3000_words.list
+	echo "$num $word"
+	convert -font $font -pointsize 150 label:$word ${num}.png
+	#curl "http://www.chinesetools.eu/names/gen_boutons.php?text=${word}&s=76&police=${police}&dispo=1" -o ${word}.png 2> /dev/null
+done < ../../map
 
