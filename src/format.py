61e4bd5371bd26e74d595db7c16876bbb34150b7
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-08-22 07:15:13 +0800

    format

--- src/format.py
+++ src/format.py
@@ -1,29 +1,42 @@
 import pickle
-import numpy
+import numpy as np
 from PIL import Image
+from os import listdir
+import argparse
 
+parser = argparse.ArgumentParser(description='')
+#parser.add_argument('-d')
+args = parser.parse_args()
 
-#im = Image.open('1.jpg').convert('LA')
-#img = numpy.asarray(img_2d) / 256
+def get_matrix(dir):
+    print dir
+    N = len(listdir(dir))
+    mat = np.zeros((N,10000))
+    label = np.zeros((N,1))
 
+    for i,file in enumerate(listdir(dir)):
+        _no = int(file.split('.')[0])
+        I = np.asarray(Image.open(dir+'/'+file))  # read image
+        v = I.ravel() / 256.  # 2-D to 1-D
+        mat[i,:] = v
+        label[i] = _no
+    print mat.shape
+    return mat, label
 
-for i in range (1,11):
-	#print i
-	infilename = "%d.jpg" % i
-	img_gray = Image.open(infilename).convert('L')
-	img_2d = numpy.array(img_gray)
-	img_1d = img_2d.ravel() / 256.
-	#print img_1d, img_1d.shape
-	if i == 1:
-		mat = img_1d
-	else:
-		mat = numpy.vstack((mat,img_1d))
+def dump_data(Data,filename):
+    f = open(filename, 'wb')
+    pickle.dump(Data, f)
+    f.close()
 
-Data = [mat, mat]
-#print mat, mat.shape
-print Data
+M = np.zeros((0,10000))
+L = np.zeros((0,1))
+#for dir in listdir('../training_data/output'):
+for dir in ["typeWQY", "type10"]:
+    mat, label = get_matrix("../training_data/output/"+dir)
+    M = np.vstack((M,mat))
+    L = np.vstack((L,label))
 
-filename = "test.pkl"
-fileObj = open(filename, 'wb')
-pickle.dump(Data, fileObj)
-fileObj.close()
+print M.shape
+print L.shape
+X = [M,L]
+dump_data(X,'WQY_10.pkl')
