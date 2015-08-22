d6346c1d599ed95bc1921f8353eada4be282f0f9
YenChen Wu <yenchen.wu.wyc@gmail.com>
2015-08-22 11:06:14 +0800

    demo

--- morphing_faces/font_morpher.py
+++ morphing_faces/font_morpher.py
@@ -1,5 +1,5 @@
 import numpy
-
+import cPickle
 numpy.random.seed(112387)
 
 
@@ -18,7 +18,7 @@ class Morpher(object):
 
         # Model parameters
         f=file('../Variational-Autoencoder/trained_params','rb')
-        [self.W1,self.W2,self.W3,self.W4,self.W5,self.W6,self.b1,self.b2,self.b3,self.b4,self.b5,self.b6] = cPickle.load(f)
+        [self.W1,self.W2,self.W3,self.W4,self.W5,self.b1,self.b2,self.b3,self.b4,self.b5] = cPickle.load(f)
 
     def toggle_freeze(self):
         """
@@ -74,16 +74,16 @@ class Morpher(object):
         """
         Maps the point Z in latent space to a 48 x 48 pixels face image
         """
-        h_decoder = T.tanh(T.dot(self.W4, self.Z) + self.b4)
-        X = T.nnet.sigmoid(T.dot(self.W5,h_decoder) + self.b5)
+        h_decoder = numpy.tanh(numpy.dot(self.W4, self.Z) + self.b4)
+        X = numpy.dot(self.W5,h_decoder) + self.b5
 
         #A_2 = numpy.dot(self.Z, self.d_W_2) + self.d_b_2
         #H_2 = numpy.where(A_2 > 0.0, A_2, 0.0 * A_2)
         #A_1 = numpy.dot(H_2, self.d_W_1) + self.d_b_1
         #H_1 = numpy.where(A_1 > 0.0, A_1, 0.0 * A_1)
         #A_0 = numpy.dot(H_1, self.d_W_0) + self.d_b_0
-        #X = 1.0 / (1.0 + numpy.exp(-A_0))
-
+        X = 1.0 / (1.0 + numpy.exp(-X))
+        print h_decoder.shape
         return X.reshape(self.image_shape)
 
     def infer_Z(self, X):
@@ -95,10 +95,10 @@ class Morpher(object):
         X : numpy.array
             Array of shape (2304, ) or (48, 48) representing the face image
         """
-        h_encoder = T.tanh(T.dot(W1,x) + b1)
-        mu_encoder = T.dot(W2,h_encoder) + b2
-        log_sigma_encoder = 0.5*(T.dot(W3,h_encoder) + b3)
-        z = mu_encoder + T.exp(log_sigma_encoder)*eps
+        h_encoder = numpy.tanh(numpy.dot(self.W1,X) + self.b1)
+        mu_encoder = numpy.dot(self.W2,h_encoder) + self.b2
+        log_sigma_encoder = 0.5*(numpy.dot(self.W3,h_encoder) + self.b3)
+        z = mu_encoder + numpy.exp(log_sigma_encoder)*eps
         
         #A_1 = numpy.dot(X, self.e_W_0) + self.e_b_0
         #H_1 = numpy.where(A_1 > 0.0, A_1, 0.0 * A_1)
